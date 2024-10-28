0e8cc65b7cc58e839109eae2da39789cff56220d
Tommy Grealy <tommygrealy@gmail.com>
2024-10-28 11:25:29 +0000

    fix placedynamite to use generic sql update method

--- py/placeDynamite.py
+++ py/placeDynamite.py
@@ -13,9 +13,13 @@ for teamName in random_teams:
     teamNextFixtureDetails = conn.get_next_fixture_for_team(teamName)
     if teamNextFixtureDetails[0][2]==teamName: # home
         print (f"Dynamite {teamName} home to {teamNextFixtureDetails[0][3]}")
-        conn.exe_sql_read(f"UPDATE `lastmanstanding`.`fixtureresults` " \
-                + f"SET `KillerTeam` = '1' WHERE (`FixtureId` = '{teamNextFixtureDetails[0][0]}');")
+        conn.update_table('fixtureresults', 
+                          update_columns={'KillerTeam': 1},
+                          filter_columns={'FixtureId': teamNextFixtureDetails[0][0]}
+                          )
     if teamNextFixtureDetails[0][3]==teamName: # away
         print (f"Dynamite {teamName} away to {teamNextFixtureDetails[0][2]}")
-        conn.exe_sql_read(f"UPDATE `lastmanstanding`.`fixtureresults` " \
-                + f"SET `KillerTeam` = '3' WHERE (`FixtureId` = '{teamNextFixtureDetails[0][0]}');")
+        conn.update_table('fixtureresults', 
+                          update_columns={'KillerTeam': 3},
+                          filter_columns={'FixtureId': teamNextFixtureDetails[0][0]}
+                          )
