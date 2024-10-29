dbeb198fc5a299b31b3854b888234fbc7c3d8438
Tommy Grealy <tommygrealy@gmail.com>
2024-10-29 21:19:37 +0000

    small fix for readability

--- py/runPostRoundResultCheck.py
+++ py/runPostRoundResultCheck.py
@@ -115,7 +115,7 @@ for user in dynamite_winners:
     success = conn.insert_into_table(
         table_name="dynamite",
         insert_columns={
-            "granted_to_user_fk":f"{usernames_ids[user['username']]}",
+            "granted_to_user_fk":f"{usernames_ids[username]}",
             "won_in_fixture_id":f"{fixtureid}"
         }
     )
