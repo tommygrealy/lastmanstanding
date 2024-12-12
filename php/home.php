68f27f3bf6eaa2d59058892338aa6392424d80be
Tommy Grealy <tommygrealy@gmail.com>
2024-12-12 10:12:05 +0000

    correct entry fee to 10

--- php/home.php
+++ php/home.php
@@ -121,7 +121,7 @@ $lms_privlevel = $user_info['PrivLevel'];
             <div data-role="content"><p>
 
                 <div data-role="fieldcontain">
-				<p style="font-weight: bold;">Entry fee is €5</p>
+				<p style="font-weight: bold;">Entry fee is €10</p>
                 Payment can be made by revolut to Tommy Grealy. <br>Revolut username: @tommy5kit <!--or online using the payment form below<br>
                     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                         <input type="hidden" name="cmd" value="_xclick">
